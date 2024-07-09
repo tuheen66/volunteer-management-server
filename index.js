b3ea189d041919cdaec379cc805d98fc5646bd6f
tuheen66 <hassan.monirul@gmail.com>
2024-07-10 03:56:32 +0600

    added api to delete volunteer request

--- index.js
+++ index.js
@@ -114,6 +114,16 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/requested/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerRequestCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
