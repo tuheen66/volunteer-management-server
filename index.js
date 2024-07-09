250951289b02555ccd0346c16bda8d90f869c5e7
tuheen66 <hassan.monirul@gmail.com>
2024-07-09 15:00:02 +0600

    added api to delete single post

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/volunteers/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
