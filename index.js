a09b76bfbed58e372547e172b27bb19f1e2b66ce
tuheen66 <hassan.monirul@gmail.com>
2024-07-09 12:34:00 +0600

    added api to get single data for view details page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -37,6 +37,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get('/volunteers/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await volunteerCollection.findOne(query);
+      res.send(result)
+    })
 
     app.post("/volunteers", async (req, res) => {
       const volunteer = req.body;
